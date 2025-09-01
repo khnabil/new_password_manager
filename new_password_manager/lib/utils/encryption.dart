f977468fc8e7943518da5285368f180e72ff1254
khnabil <nabil.khandaker@gmail.com>
2025-09-01 21:13:19 +0600

    commiting another commit

--- new_password_manager/lib/utils/encryption.dart
+++ new_password_manager/lib/utils/encryption.dart
@@ -3,38 +3,26 @@ import 'package:crypto/crypto.dart';
 import 'dart:convert';
 
 class EncryptionUtil {
-  // Helper function to derive key from password
   static encrypt.Key _generateKeyFromPassword(String password) {
-    // Generate a 256-bit key from the password using SHA-256
-    final keyBytes = utf8.encode(password); // Convert password to bytes
-    final keyHash = sha256.convert(keyBytes); // SHA-256 hash of password
-    return encrypt.Key.fromUtf8(
-      keyHash.toString().substring(0, 32),
-    ); // Use first 32 characters (256-bit)
+    final keyBytes = utf8.encode(password);
+    final keyHash = sha256.convert(keyBytes);
+    return encrypt.Key.fromUtf8(keyHash.toString().substring(0, 32));
   }
 
-  // Helper function to derive IV from password (fixed length of 16 bytes)
   static encrypt.IV _generateIVFromPassword(String password) {
-    // Use SHA-1 to create a 128-bit IV (16 bytes)
-    final ivBytes = utf8.encode(password); // Convert password to bytes
-    final ivHash = sha1.convert(ivBytes); // SHA-1 hash of password
-    return encrypt.IV.fromUtf8(
-      ivHash.toString().substring(0, 16),
-    ); // Use first 16 bytes (128-bit)
+    final ivBytes = utf8.encode(password);
+    final ivHash = sha1.convert(ivBytes);
+    return encrypt.IV.fromUtf8(ivHash.toString().substring(0, 16));
   }
 
   // Encrypt a password using a consistent key
   static String encryptPassword(String password, String masterPassword) {
-    final key = _generateKeyFromPassword(
-      masterPassword,
-    ); // Generate key from master password
-    final iv = _generateIVFromPassword(
-      masterPassword,
-    ); // Generate IV from master password
+    final key = _generateKeyFromPassword(masterPassword);
+    final iv = _generateIVFromPassword(masterPassword);
     final encrypter = encrypt.Encrypter(encrypt.AES(key));
 
     final encrypted = encrypter.encrypt(password, iv: iv);
-    return encrypted.base64; // Return the encrypted password in base64 format
+    return encrypted.base64;
   }
 
   // Decrypt a password using the same key
